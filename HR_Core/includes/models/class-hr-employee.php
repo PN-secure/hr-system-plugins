08cf21a06652a8ff362f764b8f8d8482d519c527
Rishi Dixit <rishi.dixit129@gmail.com>
2026-08-20 17:53:27 +0200

    wordpress account automatical creation, custom role display on login

--- HR_Core/includes/models/class-hr-employee.php
+++ HR_Core/includes/models/class-hr-employee.php
@@ -39,26 +39,95 @@ class HR_Employee {
     }
 
     /**
+     * Pobiera nazwę roli HR przypisanej do konta WordPress.
+     */
+    public static function get_role( $wp_user_id ) {
+        global $wpdb;
+
+        $table_employees = self::get_table_name();
+        $table_roles     = $wpdb->prefix . 'hr_roles';
+
+        $role_name = $wpdb->get_var(
+            $wpdb->prepare(
+                "SELECT r.name
+                FROM {$table_employees} e
+                INNER JOIN {$table_roles} r ON e.role_id = r.id
+                WHERE e.wp_user_id = %d
+                LIMIT 1",
+                absint( $wp_user_id )
+            )
+        );
+
+        return $role_name ? $role_name : 'employee';
+    }
+
+    /**
      * Tworzy nowego pracownika w bazie danych
      */
     public static function create( $data ) {
         global $wpdb;
         $table = self::get_table_name();
 
+        if ( get_user_by( 'email', sanitize_email( $data['email'] ) ) ) {
+            return new WP_Error(
+                'user_exists',
+                __( 'Konto WordPress z takim adresem email już istnieje.', 'hr-core' ),
+                array( 'status' => 409 )
+            );
+        }
+
+        $first_name = sanitize_text_field( $data['first_name'] );
+        $last_name  = sanitize_text_field( $data['last_name'] );
+        $full_name   = trim( $first_name . ' ' . $last_name );
+
+        $wp_user_id = wp_insert_user(
+            array(
+                'user_login'   => sanitize_email( $data['email'] ),
+                'user_pass'    => $data['password'],
+                'user_email'   => sanitize_email( $data['email'] ),
+                'first_name'   => $first_name,
+                'last_name'    => $last_name,
+                'nickname'     => $full_name,
+                'display_name' => $full_name,
+                'role'         => 'subscriber',
+            )
+        );
+
+        if ( is_wp_error( $wp_user_id ) ) {
+            return new WP_Error(
+                'user_creation_error',
+                __( 'Nie udało się utworzyć konta WordPress.', 'hr-core' ),
+                array( 'status' => 500 )
+            );
+        }
+
+        $user_nicename = sanitize_title( $full_name . '-' . $wp_user_id );
+
+        wp_update_user(
+            array(
+                'ID'           => $wp_user_id,
+                'user_nicename' => $user_nicename,
+            )
+        );
+
         $inserted = $wpdb->insert(
+
             $table,
             array(
-                'first_name'      => sanitize_text_field( $data['first_name'] ),
-                'last_name'       => sanitize_text_field( $data['last_name'] ),
+                'wp_user_id'      => $wp_user_id,
+                'first_name'      => $first_name,
+                'last_name'       => $last_name,
                 'email'           => sanitize_email( $data['email'] ),
                 'department_id'   => isset( $data['department_id'] ) ? absint( $data['department_id'] ) : null,
                 'role_id'         => isset( $data['role_id'] ) ? absint( $data['role_id'] ) : null,
                 'employment_date' => isset( $data['employment_date'] ) ? sanitize_text_field( $data['employment_date'] ) : current_time( 'mysql', 1 ),
             ),
-            array( '%s', '%s', '%s', '%d', '%d', '%s' )
+            array( '%d', '%s', '%s', '%s', '%d', '%d', '%s' )
         );
 
         if ( ! $inserted ) {
+            require_once ABSPATH . 'wp-admin/includes/user.php';
+            wp_delete_user( $wp_user_id );
             return false;
         }
 
